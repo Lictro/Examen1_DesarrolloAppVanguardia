2e0912c8c81c3ba9d99f23945e9c85c6784aef73
Pavon <jcarias199546@unitec.edu>
2018-05-25 21:02:40 -0600

    Button Added

--- src/Contact.js
+++ src/Contact.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { string, object, func } from 'prop-types';
 import './Contact.css';
 
+function deleteEle(){
+
+}
+
 class Contact extends Component {
     render() {
     const { name, email, phone, twitter } = this.props;
     return (
       <div className="Contact">
         <h3>{name} - {email} - {phone} - {twitter}</h3>
+        <button onClick={deleteEle()}>Eliminar</button>  
       </div>
     );
   }
